f7d758e51648fb475ce8b9b15bae95d2dc032f88
Nguyen Thanh Bien <biennt0979@gmail.com>
2025-04-18 13:53:01 +0700

    Update ollama-chatbot.py

--- ollama-chatbot.py
+++ ollama-chatbot.py
@@ -1,22 +1,19 @@
-import os
 from openai import OpenAI
 import gradio as gr
 
 #########################################
 def chat(message, history):
-    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    #messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    messages = history + [{"role": "user", "content": message}]
     response = openai.chat.completions.create(model=MODEL, messages=messages)
     return response.choices[0].message.content
 
 ## Main
 api_key = "ollama"
-base_url = "http://192.168.0.2:11434"
-
-MODEL="deepseek-r1:8b"
-
-system_message = ""
+base_url = "http://127.0.0.1:11434"
+MODEL="myllama"
+#system_message = "You are a helpful assistant."
 
 openai  = OpenAI(base_url = base_url + "/v1", api_key = api_key)
-
-demo = gr.ChatInterface(fn=chat, type="messages", title="AI Chatbot - Generic")
+demo = gr.ChatInterface(fn=chat, type="messages", title="AI Chatbot - " + MODEL + " from " + base_url)
 demo.launch()
